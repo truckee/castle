567ca2bd1492f116af65cf0cc6e550adbfc24841
George Brooks <truckeesolutions@gmail.com>
2025-01-27 14:34:34 -0800

    images on wider windows

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,1 +1,13 @@
 $('#nav-placeholder').load('../templates/navbar.html');
+
+function updateBodyClass() {
+document.addEventListener('DOMContentLoaded', function() {
+    const width = window.innerWidth;
+    if (width < 760) {
+     document.body.className = 'small-screen';
+    }
+  });
+}
+
+updateBodyClass(); // Call the function initially
+window.addEventListener('load', updateBodyClass); // Update on page load (for SPAs)
