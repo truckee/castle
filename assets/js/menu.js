a5abba2f5a159c368c9e9af9156784902204e3e2
George Brooks <truckeesolutions@gmail.com>
2025-01-28 16:10:51 -0800

    screen min 1200

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,7 +3,7 @@ $('#nav-placeholder').load('../templates/navbar.html');
 function updateBodyClass() {
 document.addEventListener('DOMContentLoaded', function() {
     const width = window.innerWidth;
-    if (width < 480) {
+    if (width < 1200) {
      document.body.className = 'small-screen';
     }
   });
