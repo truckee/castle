3ee957fc20081335400cb6f924d23ef7d53a9bc4
George Brooks <truckeesolutions@gmail.com>
2025-12-08 07:17:05 -0800

    fix text & menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,12 @@
-/* $('#nav-placeholder').load('../templates/navbar.html'); */
+$('#nav-placeholder').load('../templates/navbar.html');
 
-const url = window.location.pathname;
+/* const url = window.location.pathname;
 const htmlFileName = url.substring(url.lastIndexOf('/') + 1);
 textfile = '../text/text_'+ htmlFileName;
 $('#menu').load('../templates/navbar.html');
 $('#placeholder').load(textfile);
 console.log(htmlFileName);
-
+ */
 function updateBodyClass() {
 document.addEventListener('DOMContentLoaded', function() {
     const width = window.innerWidth;
